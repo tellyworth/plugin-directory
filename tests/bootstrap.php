62b72df51e6159b552606fe282dbc3187463b8f9
tellyworth <alex@automattic.com>
2021-06-02 17:18:37 +1000

    Make unit tests runnable via composer
    ie `composer test`

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,9 +2,18 @@
 
 namespace WordPressdotorg\Plugin_Directory\Tests;
 
+// Require composer dependencies.
+require_once dirname( __DIR__ ) . '/vendor/autoload.php';
+
 if ( 'cli' !== php_sapi_name() ) {
 	return;
 }
+
+$_tests_dir = getenv( 'WP_PHPUNIT__DIR' );
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
 
 /**
  * Manually load the plugin being tested.
@@ -12,5 +21,9 @@ if ( 'cli' !== php_sapi_name() ) {
 function manually_load_plugin() {
 	require_once dirname( __FILE__ ) . '/../plugin-directory.php';
 }
+if ( function_exists( 'tests_add_filter' ) ) {
+	tests_add_filter( 'muplugins_loaded', __NAMESPACE__ . '\manually_load_plugin' );
+}
 
-tests_add_filter( 'muplugins_loaded', __NAMESPACE__ . '\manually_load_plugin' );
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
